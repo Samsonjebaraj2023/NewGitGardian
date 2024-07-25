47860bab83473be4bc143c46f77b105d9eedf488
Samson jebaraj <124692671+Samsonjebaraj2023@users.noreply.github.com>
2024-07-25 10:54:00 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 [default]
-AWS_SCRET_ID_KEY = AKIAXYKJVBA2ELRGLTZEx
-AWS_Scret_KEY = uDrR5awKGsLLCX/say4A8/z1IxLI8N16x/91K+TC
+AWS_SCRET_ID_KEY = 
+AWS_Scret_KEY = 
 output = json 
 region = us-east-1
